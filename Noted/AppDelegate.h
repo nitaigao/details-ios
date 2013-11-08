5b31dea220880bfbe933fc8d0f23da03aaa8da37
Nicholas Kostelnik <nkostelnik@gmail.com>
2013-11-08 08:20:17 +0000

    Initial Commit

---
+++ Noted/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Noted
+//
+//  Created by Nicholas Kostelnik on 08/11/2013.
+//  Copyright (c) 2013 Nicholas Kostelnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
